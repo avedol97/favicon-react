0ef241e0dbbe9e23cc06d2763328e7e9e4a90c0d
krystian <krystian.cwioro@o2.pl>
2021-01-30 01:05:08 -0800

    map

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './ListItem.css';
-import danabramovImage from '../../assets/Images/danabramov.jpg'
 
 const ListItem = (props) =>(
 <li className="listItem__wrapper">
-    <img src={props.image}  className="listItem__image"/>
+    <img
+        alt={props.name}
+        src={props.image}
+        className="listItem__image"/>
     <div>
         <h2 className="listItem__name">{props.name}</h2>
         <p className="listItem__description">
             {props.description}</p>
-        <button className="listItem__button">visit twitter page</button>
+        <a href={props.link} className="listItem__button">visit twitter page</a>
     </div>
 </li>
 )
