b443d0b9dc594e0c13cc5a8ad775ef90ff064e26
krystian <krystian.cwioro@o2.pl>
2021-01-30 01:26:38 -0800

    PropTypes

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import './ListItem.css';
+import * as PropTypes from "prop-types";
 
-const ListItem = (image,name,description,twitterLink) =>(
-<li className="listItem__wrapper">
-    <img
-        alt={name}
-        src={image}
-        className="listItem__image"/>
-    <div>
-        <h2 className="listItem__name">{props.name}</h2>
-        <p className="listItem__description">
-            {description}</p>
-        <a href={twitterLink} className="listItem__button">visit twitter page</a>
-    </div>
-</li>
+const ListItem = ({
+                      image,
+                      name,
+                      description,
+                      twitterLink
+                  }) => (
+    <li className="listItem__wrapper">
+        <img
+            alt={name}
+            src={image}
+            className="listItem__image"/>
+        <div>
+            <h2 className="listItem__name">{name}</h2>
+            <p className="listItem__description">
+                {description}</p>
+            <a href={twitterLink} className="listItem__button">visit twitter page</a>
+        </div>
+    </li>
 )
 export default ListItem;
+
+ListItem.propTypes = {
+    image:PropTypes.string.isRequired,
+    name:PropTypes.string.isRequired,
+    description:PropTypes.string,
+    twitterLink:PropTypes.string.isRequired,
+}
+
+ListItem.defaultProps ={
+    description: "One of the React Creators",
+}
