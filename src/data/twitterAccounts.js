729d55bc7c861bd7635f7fdce4f2d064f5f35341
krystian <krystian.cwioro@o2.pl>
2021-01-30 00:48:45 -0800

    learn

---
+++ src/data/twitterAccounts.js
@@ -0,0 +1,33 @@
+import danabramovImage from '../assets/Images/danabramov.jpg'
+import kentdoddsImage from '../assets/Images/kentdodds.jpg'
+import michaeljacksonImage from '../assets/Images/michaeljackson.jpg'
+import ryanflorenceImage from '../assets/Images/ryanflorence.jpg'
+
+
+export const twitterAccounts = [
+{
+    image:danabramovImage,
+    name:'Dan Abramov',
+    description:'Dan Abramov is a software engineer at Facebook.\n' +
+        '            Together with Andrew Clark, he created Redux.',
+    twitterLink:'https://twitter.com/dan_abramov',
+},
+    {
+        image:ryanflorenceImage,
+        name:'Ryan Florence',
+        description:'Ryan started making websites for a local ISP before JavaScript or CSS were created.',
+        twitterLink:'https://twitter.com/ryanflorence',
+    },
+    {
+        image:michaeljacksonImage,
+        name:'Michael Jackson',
+        description:'Building @remix_run. Built @unpkg, 2B+ hits per day.',
+        twitterLink:'https://twitter.com/mjackson',
+    },
+    {
+        image:kentdoddsImage,
+        name:'Kent C. Dodds',
+        description:'Hi, Im Kent C. Dodds. I help people make the word better through quality software',
+        twitterLink:'https://twitter.com/kentcdodds',
+    },
+]
